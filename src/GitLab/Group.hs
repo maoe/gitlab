e7d7d2ca4d85125b1fcd8792ce278cac2ba77344
Mitsutoshi Aoe <maoe@foldr.in>
2013-10-08 23:55:08 +0900

    Add getGroup

--- src/GitLab/Group.hs
+++ src/GitLab/Group.hs
@@ -1,15 +1,28 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
 module GitLab.Group where
+import Data.Monoid ((<>))
+import qualified Data.Text.Encoding as TE
+
 import Data.Conduit
 import Network.HTTP.Conduit
+import Web.PathPieces (toPathPiece)
 
 import GitLab.Types
-import GitLab.Rest (restSource)
+import GitLab.Rest (rest, restSource)
 
 listGroups
   :: (MonadBaseControl IO m, MonadResource m)
   => Source (GitLabT m) Group
 listGroups = restSource $ \request -> request
   { path = "/groups"
+  }
+
+getGroup
+  :: (MonadBaseControl IO m, MonadResource m)
+  => GroupId
+  -> GitLabT m (Maybe Group)
+getGroup groupId = rest $ \request -> request
+  { method = "GET"
+  , path = TE.encodeUtf8 $ "/groups/" <> toPathPiece groupId
   }
