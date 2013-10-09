b735cff5d4175eb70ecc38641fa050cce704fab2
Mitsutoshi Aoe <maoe@foldr.in>
2013-10-09 20:18:44 +0900

    Add getProjectSnippet

--- src/GitLab/ProjectSnippet.hs
+++ src/GitLab/ProjectSnippet.hs
@@ -1,20 +1,34 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
 module GitLab.ProjectSnippet where
-import Data.Monoid ((<>))
+import Data.Monoid ((<>), mconcat)
 import qualified Data.Text.Encoding as TE
 
 import Data.Conduit
 import Network.HTTP.Conduit
 import Web.PathPieces (toPathPiece)
 
-import GitLab.Rest (restSource)
+import GitLab.Rest (rest, restSource)
 import GitLab.Types
 
-listSnippets
+listProjectSnippets
   :: (MonadBaseControl IO m, MonadResource m)
   => ProjectId
   -> Source (GitLabT m) Snippet
-listSnippets projectId = restSource $ \request -> request
+listProjectSnippets projectId = restSource $ \request -> request
   { path = TE.encodeUtf8 $ "/projects/" <> toPathPiece projectId <> "/snippets"
+  }
+
+getProjectSnippet
+  :: (MonadBaseControl IO m, MonadResource m)
+  => ProjectId
+  -> SnippetId
+  -> GitLabT m (Maybe Snippet)
+getProjectSnippet projectId snippetId = rest $ \request -> request
+  { path = TE.encodeUtf8 $ mconcat
+      [ "/projects/"
+      , toPathPiece projectId
+      , "/snippets/"
+      , toPathPiece snippetId
+      ]
   }
